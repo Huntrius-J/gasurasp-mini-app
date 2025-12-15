f6c544f7db132def5d3de77cfd4ecbf9cb1aa58d
huntrius <huntrhomka@gmail.com>
2025-12-15 16:12:24 +0700

    fix launch in max

--- src/main.js
+++ src/main.js
@@ -4,7 +4,16 @@ import { AppConfig } from './AppConfig.js';
 
 vkBridge.send('VKWebAppInit');
 
+handleMaxAppParameters() 
 createRoot(document.getElementById('root')).render(<AppConfig />);
+
+function handleMaxAppParameters() {
+  window.history.replaceState(
+    null, 
+    null, 
+    window.location.hash = '#/'
+  );
+}
 
 if (import.meta.env.MODE === 'development') {
   import('./eruda.js');
